1d7fc84ff132bbf24dad896cb05c95021e152a79
Ben Waldman <benmw2@gmail.com>
2023-02-08 18:16:20 -0600

    removing old functions

--- day9/index.js
+++ day9/index.js
@@ -17,44 +17,6 @@ async function processLineByLine() {
   return inputArray;
 }
 
-//find size of grid
-//count maximum right while keeping count of current right position, same for up
-function findGridSize(input) {
-    let maxRight = 0;
-    let maxUp = 0;
-    let currentX = 0;
-    let currentY = 0;
-    input.forEach(line => {
-        let [direction, distance] = line.split(' ')
-        distance = parseInt(distance, 10)
-        if(direction === "R") {
-            currentX = currentX + distance;
-            if(currentX > maxRight) {
-                maxRight = currentX;
-            }
-        }
-        if(direction === "L") {
-            currentX = currentX - distance;
-        }
-        if(direction === "U") {
-            currentY = currentY + distance;
-            if(currentY > maxUp) {
-                maxUp = currentY;
-            }
-        }
-        if(direction === "D") {
-            currentY = currentY - distance;
-        }
-    })
-    return[maxRight, maxUp]
-}
-
-function createGrid(x,y) {
-    let array = Array(y)
-    array.fill(Array(x).fill(0))
-    return array
-}
-
 function traverseGrid( input) {
     let coordSet = new Set()
     let currentHeadX = 0;
@@ -64,10 +26,6 @@ function traverseGrid( input) {
     let currentTailX = 0;
     let currentTailY = 0;
     input.forEach(line => {
-        //move head function
-
-        //do each step individually
-        //have last head position so tail can jump there
         let [direction, distance] = line.split(' ')
         distance = parseInt(distance, 10)
         for(let i = 1; i <= distance; i++) {
@@ -98,8 +56,6 @@ function traverseGrid( input) {
 
 async function main() {
     let input = await processLineByLine();
-    // let [x,y] = findGridSize(input)
-    // let grid = createGrid(x,y)
     traverseGrid(input)
 }
 
