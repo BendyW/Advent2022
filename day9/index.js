51c9de2d96fc790bbf29513448aa21b2f61f8cc3
Ben Waldman <benmw2@gmail.com>
2023-02-13 16:12:49 -0600

    fixing direction computation for tails

--- day9/index.js
+++ day9/index.js
@@ -17,7 +17,7 @@ async function processLineByLine() {
   return inputArray;
 }
 
-function traverseGrid( input) {
+function traverseGrid(input) {
     let coordSet = new Set()
     let currentHeadX = 0;
     let currentHeadY = 0;
@@ -54,9 +54,48 @@ function traverseGrid( input) {
     console.log(coordSet.size)
 }
 
+function traverseGrid2(input, length) {
+    let coordSet = new Set()
+    let rope = Array.from({length: length}, _ => ({x: 0, y: 0}))
+    input.forEach(line => {
+        let [direction, distance] = line.split(' ')
+        distance = parseInt(distance, 10)
+        const [head, tail] = [rope[0], rope.slice(-1)[0]]
+        for(let i = 1; i <= distance; i++) {
+            //find head pos
+            if(direction === "R") {
+                head.x++
+            }
+            if(direction === "L") {
+                head.x--
+            }
+            if(direction === "U") {
+                head.y++
+            }
+            if(direction === "D") {
+                head.y--
+            }
+
+            //find positions of each part of rope after head
+            for (let i = 1; i < rope.length; i++) {
+                const [prev, curr] = [rope[i - 1], rope[i]]
+                if(Math.abs(prev.x - curr.x) >= 2 || Math.abs(prev.y - curr.y) >= 2) { 
+                    let directionX = prev.x - curr.x
+                    let directionY = prev.y - curr.y
+                    curr.x += Math.abs(directionX) === 2 ? directionX / 2 : directionX;
+                    curr.y += Math.abs(directionY) === 2 ? directionY / 2 : directionY;
+                }
+            }
+            coordSet.add(`${tail.x},${tail.y}`)
+        }
+    })
+    console.log(coordSet.size)
+}
+
 async function main() {
     let input = await processLineByLine();
-    traverseGrid(input)
+    // traverseGrid(input)
+    traverseGrid2(input, 10)
 }
 
 main()
