78d7bf145ba6103d9e25625d2a8678216c3bceec
Ben Waldman <benmw2@gmail.com>
2023-03-02 11:32:06 -0600

    solution to part 1 completed

--- day11/index.js
+++ day11/index.js
@@ -8,7 +8,7 @@ async function processInput() {
     .map((monkey) => {
       const monkeyNumberRegex = /Monkey ([0-9]+):/
       const monkeyItemsRegex = /Starting items: (.*)/
-      const monkeyOperationRegex = /Operation: new = old (.*)/
+      const monkeyOperationRegex = /Operation: new = old (.) (.*)/
       const monkeyTestRegex = /Test: divisible by ([0-9]+)/
       const monkeyTrueRegex = /If true: throw to monkey ([0-9]+)/
       const monkeyFalseRegex = /If false: throw to monkey ([0-9]+)/
@@ -16,12 +16,12 @@ async function processInput() {
       let monkeyID = monkey.match(monkeyNumberRegex)[1]
       let newMonkey = new Monkey(monkeyID);
 
-      newMonkey.itemsArray = monkey.match(monkeyItemsRegex)[1].split(', ')
+      newMonkey.itemsWorryLevelArray = monkey.match(monkeyItemsRegex)[1].split(', ').map(x => parseInt(x, 10))
       newMonkey.testDivisibleBy = monkey.match(monkey.match(monkeyTestRegex)[1])
-      newMonkey.ifTrue =  monkey.match(monkey.match(monkeyTrueRegex)[1])
-      newMonkey.ifFalse = monkey.match(monkey.match(monkeyFalseRegex)[1])
-      let monkeyOperation = monkey.match(monkeyOperationRegex)[1]
-      newMonkey.operations = operation(monkeyOperation)
+      newMonkey.ifTrueMonkeyID =  parseInt(monkey.match(monkey.match(monkeyTrueRegex)[1]), 10)
+      newMonkey.ifFalseMonkeyID = parseInt(monkey.match(monkey.match(monkeyFalseRegex)[1]), 10)
+      newMonkey.operatorOperand = monkey.match(monkeyOperationRegex)[1]
+      newMonkey.operatorValue = monkey.match(monkeyOperationRegex)[2]
 
       monkeys.push(newMonkey);
     })
@@ -32,49 +32,102 @@ async function processInput() {
 class Monkey {
   constructor(monkeyID){
     this.monkeyID = monkeyID;
+    this.itemsInspected = 0;
   }
   itemsWorryLevelArray;
   testDivisibleBy;
-  operations;
+  ifTrueMonkeyID;
+  ifFalseMonkeyID;
+  operatorOperand;
+  operatorValue;
 
-  processItems() {
-    this.monkey.itemsWorryLevelArray.forEach((itemWorryLevel, itemIndex) => {
-      itemWorryLevel = this.monkey.operations()
-      itemWorryLevel = itemWorryLevel / 3
-      let isTestTrue = (this.monkey.itemWorryLevel % this.monkey.testDivisibleBy === 0) ? true : false;
+  worryLevelOperation(currentWorryLevel, operatorOperand, operatorValue) {
+    //Convert operatorValue to Int and check if valid
+    let operatorInt = parseInt(operatorValue, 10)
+    if(isNaN(operatorInt)) {
+      if(operatorValue === "old") {
+        operatorInt = currentWorryLevel
+      } else {
+        throw new Error(`Invalid operand ${operatorValue}`)
+      }
+    }
+    let newWorryLevel = 0;
+
+    switch (operatorOperand) {
+      case "*":
+        newWorryLevel = currentWorryLevel * operatorInt 
+        break;
+      case "+":
+        newWorryLevel = currentWorryLevel + operatorInt 
+        break;
+      default:
+        throw new Error(`Invalid Operation: ${operatorOperand}`)
+    }
+    
+    return newWorryLevel
+  }
+
+  processItems(monkeys) {
+    this.itemsInspected += this.itemsWorryLevelArray.length;
+    while(this.itemsWorryLevelArray.length) {
+      //Get next item
+      let itemWorryLevel = this.itemsWorryLevelArray.shift()
+      //Run Operation to calculate new worry level
+      itemWorryLevel = this.worryLevelOperation(itemWorryLevel, this.operatorOperand, this.operatorValue)
+      itemWorryLevel = Math.floor(itemWorryLevel / 3)
+      // Run test to see which monkey to pass to
+      let isTestTrue = (itemWorryLevel % this.testDivisibleBy === 0) ? true : false;
 
       if(isTestTrue) {
-        //splice to try
+        monkeys[this.ifTrueMonkeyID].itemsWorryLevelArray.push(itemWorryLevel)
       } else {
-        //splice to false
+        monkeys[this.ifFalseMonkeyID].itemsWorryLevelArray.push(itemWorryLevel)
       }
       
-    })
+    }
   }
-}
 
-function operation(operation) {
-  let opererands = operands.split(' ')
-
-  return newValue
+  toString() {
+    let returnString = "";
+    returnString = `Monkey ID: ${this.monkeyID}\n`
+    returnString += `Monkey Items: ${this.itemsWorryLevelArray}`
+    return returnString
+  }
 }
 
 async function main() {
-  const totalRounds = 1
+  if(isDebug) console.log("Starting Keep Away")
+  const totalRounds = 20
   let itemsInspected = 0
   let monkeys = await processInput();
 
-  for(let round = 1; i <= totalRounds; round++) {
+  for(let round = 1; round <= totalRounds; round++) {
+    if(isDebug) console.log(`Starting Round ${round}`)
     monkeys.forEach(monkey => {
-      itemsInspected += monkey.items.length;
-      monkey.processItems();
-
-
+      itemsInspected += monkey.itemsWorryLevelArray.length;
+      monkey.processItems(monkeys);
     });
-
+    if(isDebug) {
+      monkeys.forEach(monkey => {
+        console.log(monkey.toString())
+        console.log(`Monkey items inspected: ${monkey.itemsInspected}`)
+      })
+    }
   }
-
+  
+  let part1HighestValues = []
+  let part1Solution = 0;
+  monkeys.forEach(monkey => {
+    part1HighestValues.push(monkey.itemsInspected)
+    console.log(typeof monkey.itemsInspected)
+  })
+  part1HighestValues.sort(function (a, b) {  return a - b;  })
+  part1HighestValues = part1HighestValues.slice(Math.max(part1HighestValues.length - 2, 0))
+  part1Solution = part1HighestValues[0] * part1HighestValues[1]
+  console.log(part1Solution)
 
 }
 
+
+let isDebug = true;
 main()
